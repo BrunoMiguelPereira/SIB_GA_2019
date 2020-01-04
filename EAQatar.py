0c2ba527ffdf42c0aba00f5a02765c6d6e777356
BrunoPeras <46814163+BrunoPeras@users.noreply.github.com>
2020-01-04 18:35:02 +0000

    Add files via upload

---
+++ EAQatar.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 15:51:29 2019
+
+@author: Asus
+"""
+from EvolAlgorithm import EvolAlgorithm
+from Popul import  PopulPermut
+from math import sqrt as sq
+
+#Ler o ficheiro com os dados das cidades
+def read_file(filename):
+    try:
+        cities = {}
+        with open(filename,"r") as file:
+            lines = file.readlines()
+            dim = lines[4].rstrip().split()[2]
+            for i in range(7,len(lines)-1):
+                line = lines[i].rstrip().split()
+                cities[int(line[0])] = (float(line[1]),float(line[2]))
+            file.close()
+        return cities,dim
+    except IOError:
+        print("Ficheiro não encontrado.")
+    else:
+        print("Ficheiro lido com sucesso.\n")
+
+#Função para calcular a distânica (euclidiana)      
+def distancia(a,b):
+    return sq((a[0]-b[0])**2 + (a[1]-b[1])**2)
+  
+#Armazenar e devolver um dicionario de dicionários
+#cidade1 -> {cidade2:dist}      
+def distance_matrix(dicionario):
+    D = {}
+    for city1,cords1 in dicionario.items():
+        D[city1] = {}
+        for city2,cords2 in dicionario.items():
+            D[city1][city2] = distancia(cords1,cords2)
+    return D
+
+
+
+class EvoQatar(EvolAlgorithm):
+    
+    #inicializar a classe para o problema
+    def __init__(self,popsize,numits,noffspring,indsize,dist_matrix):
+        self.popsize = popsize
+        self.numits = numits
+        self.noffspring = noffspring
+        self.indsize = indsize
+        self.dist_matrix = dist_matrix
+    
+    #inicializar a população com representação permutável aleatoriamente
+    def initPopul(self, indsize):
+        self.popul = PopulPermut(self.popsize, indsize)
+    
+    #faz uma geração, seleciona os pais, gera os filhos sendo que há thresholds 
+    #que alteram a forma como os filhos são gerados para quebrar uma população
+    #idêntica
+    def iteration(self):
+        parents = self.popul.selection(self.noffspring)
+        if not self.popul.bestFitness()>20000:
+            offspring = self.popul.recombination(parents, self.noffspring)
+            self.evaluate(offspring)
+        elif 15000<self.popul.bestFitness()<20000:
+            offspring = self.popul.recombination(parents, self.noffspring,True)
+            self.evaluate(offspring)
+        elif self.popul.bestFitness()<15000:
+            offspring = self.popul.recombination(parents, self.noffspring)
+            self.evaluate(offspring)
+        else:
+            offspring = self.popul.recombination(parents, self.noffspring,True)
+            self.evaluate(offspring)
+        self.popul.reinsertion(offspring)
+    
+    #estabelece a fitness para um conjunto dos indivíduos
+    def evaluate(self,indivs):
+        for i in range(len(indivs)):
+            ind = indivs[i]
+            fit = 0.0
+            genes = ind.getGenes()
+            for i in range(0,len(genes)-1):
+                fit += self.dist_matrix[genes[i]][genes[i+1]]
+            ind.setFitness(fit)
+        return None
+    
+    
+    #corre o algoritmo evolucionário
+    def run(self):
+        self.initPopul(self.indsize)
+        self.evaluate(self.popul.indivs)
+        self.bestsol = []
+        self.bestfit = 99999999999999999999
+        for i in range(self.numits+1):            
+            self.iteration()
+            bs, bf = self.popul.bestSolution()
+            if bf < self.bestfit:
+                self.bestfit = bf
+                self.bestsol = bs
+            print("Iteration:", i, " ", "Best: ", self.popul.bestFitness())
+
+    
+    def printBestSolution(self):
+        print("Best solution: ", self.bestsol.getGenes())
+        print("Best fitness:", self.bestfit)
+
+
+def menu():
+    objeto = None
+    ind_dim = 0
+    pop_size = 50
+    n_offsp = 26
+    max_iterations = 1000
+    matriz = None
+    while True:
+        print('''\nBem-vindo ao algorítmo genético com o objetivo de encontrar o caminho mais curto que atravessa todas as cidades de um país apenas 1x.
+        Disponibilizamos as seguintes opções:
+                  1. Ler o ficheiro com a informação do país (TSPLIB Format)
+                  2. Ler parâmetros para correr o algoritmo genético (se não quiser modificar corra o algoritmo)
+                  3. Correr algoritmo genético
+                  4. Sair''')
+        op = input("Opção pretendida? ")
+        if op == "1":
+            try:
+                filename = input("Nome do ficheiro a analisar (não se esqueça da extensão): ")
+                cidades,dim = read_file(filename)
+                ind_dim = int(dim)+1
+                matriz = distance_matrix(cidades)
+            except:
+                raise
+            else:
+                print("\nFicheiro lido com sucesso.\n")
+        elif op == "2":
+            try:
+                popsize_i = int(input("Tamanho da população (Enter para default)? "))
+            except ValueError:
+                pop_size = 50
+            else:
+                pop_size = popsize_i
+            try:
+                n_offspring = int(input("Número de filhos por geração (Enter para default)? "))
+            except ValueError:
+                n_offsp = 26
+            else:
+                n_offsp = n_offspring
+            try:
+                n_iter = int(input("Número de iterações de todo o processo a realizar (Enter para default)? "))
+            except ValueError:
+                max_iterations = 1000
+            else:
+                max_iterations = n_iter
+        elif op == "3":
+            objeto = EvoQatar(pop_size,max_iterations,n_offsp,ind_dim,matriz)
+            objeto.run()
+            objeto.printBestSolution()            
+        elif op == "4":
+            break
+    print("Adeus, Obrigado!")
+                
+            
+            
+            
+            
+        
+if __name__=="__main__":
+    menu()
+
+
+
+
+    
+    
+
---
+++ Indiv.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+from random import randint, random, shuffle, choice
+import numpy as np
+
+
+
+
+class Indiv:
+    
+    def __init__(self, size, genes = []):
+        if genes: self.genes = genes 
+        else: self.initRandom(size)
+        self.fitness = None
+        
+    def setFitness(self, fit):
+        self.fitness = fit
+
+    def getFitness(self):
+        return self.fitness
+    
+    def getGenes(self):
+        return self.genes
+    
+    def initRandom(self, size):
+        self.genes = []
+        for i in range(size):
+            self.genes.append(randint(0,1))
+            
+    def mutation(self):
+        s = len(self.genes)
+        pos = randint(0,s-1)
+        if self.genes[pos] == 0: self.genes[pos] = 1
+        else: self.genes[pos] = 0
+    
+    def crossover(self, indiv2):
+        return self.one_pt_crossover(indiv2)
+    
+    def one_pt_crossover(self, indiv2):
+        offsp1 = []
+        offsp2 = []
+        s = len(self.genes)
+        pos = randint(0,s-1)
+        for i in range(pos):
+            offsp1.append(self.genes[i])
+            offsp2.append(indiv2.genes[i])
+        for i in range(pos, s):
+            offsp2.append(self.genes[i])
+            offsp1.append(indiv2.genes[i])
+        return Indiv(s, offsp1), Indiv(s, offsp2)
+    
+
+
+class IndivPermut (Indiv):
+
+    def __init__(self, size = 195, genes = []):
+        self.size = size
+        if genes: self.genes = genes
+        else: self.initRandomPer()
+        self.fitness = None
+    
+    def initRandomPer(self):
+        genes = np.random.permutation(self.size)
+        self.genes = genes[genes != 0].tolist()
+        return self.genes
+        
+    #mutação para causar variabilidade, troca de 2 cidades de posição
+    def mutation(self):
+        nind = len(self.genes)
+        i = 0
+        j = 0
+        while i==j:
+            i = randint(0,nind-1)
+            j = randint(0,nind-1)
+        temp = self.genes[i]
+        self.genes[i] = self.genes[j]
+        self.genes[j] = temp
+        
+
+    
+    def crossover(self, indiv2,cl=False):
+        return self.one_pt_crossover(indiv2,cl)
+    
+    #método para realizar o cruzamento de filhos
+    #utiliza o parâmetro cl para causar uma mutação maior nos filhos
+    def one_pt_crossover(self,indiv2,cl=False):
+        s = len(self.genes)
+        pos = randint(0,s-1)
+        offsp1 = list(self.getGenes())
+        offsp2 = list(indiv2.getGenes())
+        if not cl:
+            offsp1[pos:] = sorted(offsp1[pos:],key=lambda x:indiv2.genes)
+            offsp2[pos:] = sorted(offsp2[pos:],key=lambda x:self.genes)
+        else:
+            l = [True,False]
+            ck = choice(l)
+            if not ck:
+                offsp1[pos:] = sorted(offsp1[pos:])
+                offsp2[:pos] = sorted(offsp2[:pos],reverse=True)
+            else:
+                offsp2[pos:] = sorted(offsp2[pos:])
+                offsp1[:pos] = sorted(offsp1[:pos],reverse=True)
+
+        return IndivPermut(s, offsp1), IndivPermut(s, offsp2)
+    
+
+
+
